7fe30edc42f4ed5c8e7bf1c2674f7e6c09757bd5
Cayla07 <maicaeupinado@gmail.com>
2025-02-14 23:28:33 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace EUPINADO
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            Tbx_Equation = new TextBox();
+            Tbx_Result = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button17 = new Button();
+            button18 = new Button();
+            button19 = new Button();
+            button20 = new Button();
+            SuspendLayout();
+            // 
+            // Tbx_Equation
+            // 
+            Tbx_Equation.BackColor = SystemColors.InactiveCaption;
+            Tbx_Equation.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Tbx_Equation.Location = new Point(12, 12);
+            Tbx_Equation.Multiline = true;
+            Tbx_Equation.Name = "Tbx_Equation";
+            Tbx_Equation.Size = new Size(530, 65);
+            Tbx_Equation.TabIndex = 0;
+            Tbx_Equation.TabStop = false;
+            Tbx_Equation.Text = "0";
+            // 
+            // Tbx_Result
+            // 
+            Tbx_Result.BackColor = SystemColors.InactiveCaption;
+            Tbx_Result.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Tbx_Result.Location = new Point(12, 79);
+            Tbx_Result.Multiline = true;
+            Tbx_Result.Name = "Tbx_Result";
+            Tbx_Result.Size = new Size(530, 57);
+            Tbx_Result.TabIndex = 1;
+            Tbx_Result.TabStop = false;
+            Tbx_Result.Text = "0";
+            Tbx_Result.TextAlign = HorizontalAlignment.Right;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.SteelBlue;
+            button1.Location = new Point(12, 248);
+            button1.Name = "button1";
+            button1.Size = new Size(128, 100);
+            button1.TabIndex = 2;
+            button1.Text = "7";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += NumBtn_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.SteelBlue;
+            button2.Location = new Point(146, 248);
+            button2.Name = "button2";
+            button2.Size = new Size(128, 100);
+            button2.TabIndex = 3;
+            button2.Text = "8";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += NumBtn_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.SteelBlue;
+            button3.Location = new Point(280, 248);
+            button3.Name = "button3";
+            button3.Size = new Size(128, 100);
+            button3.TabIndex = 4;
+            button3.Text = "9";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += NumBtn_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.LightSkyBlue;
+            button4.Location = new Point(414, 248);
+            button4.Name = "button4";
+            button4.Size = new Size(128, 100);
+            button4.TabIndex = 5;
+            button4.Text = "*";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += OperatorBtn_Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.SteelBlue;
+            button5.Location = new Point(12, 354);
+            button5.Name = "button5";
+            button5.Size = new Size(128, 100);
+            button5.TabIndex = 6;
+            button5.Text = "4";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += NumBtn_Click;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.SteelBlue;
+            button6.Location = new Point(146, 354);
+            button6.Name = "button6";
+            button6.Size = new Size(128, 100);
+            button6.TabIndex = 7;
+            button6.Text = "5";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += NumBtn_Click;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.SteelBlue;
+            button7.Location = new Point(280, 354);
+            button7.Name = "button7";
+            button7.Size = new Size(128, 100);
+            button7.TabIndex = 8;
+            button7.Text = "6";
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += NumBtn_Click;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.LightSkyBlue;
+            button8.Location = new Point(414, 354);
+            button8.Name = "button8";
+            button8.Size = new Size(128, 100);
+            button8.TabIndex = 9;
+            button8.Text = "-";
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += OperatorBtn_Click;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.SteelBlue;
+            button9.Location = new Point(12, 460);
+            button9.Name = "button9";
+            button9.Size = new Size(128, 100);
+            button9.TabIndex = 10;
+            button9.Text = "1";
+            button9.UseVisualStyleBackColor = false;
+            button9.Click += NumBtn_Click;
+            // 
+            // button10
+            // 
+            button10.BackColor = Color.SteelBlue;
+            button10.Location = new Point(146, 460);
+            button10.Name = "button10";
+            button10.Size = new Size(128, 100);
+            button10.TabIndex = 11;
+            button10.Text = "2";
+            button10.UseVisualStyleBackColor = false;
+            button10.Click += NumBtn_Click;
+            // 
+            // button11
+            // 
+            button11.BackColor = Color.SteelBlue;
+            button11.Location = new Point(280, 460);
+            button11.Name = "button11";
+            button11.Size = new Size(128, 100);
+            button11.TabIndex = 12;
+            button11.Text = "3";
+            button11.UseVisualStyleBackColor = false;
+            button11.Click += NumBtn_Click;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.LightSkyBlue;
+            button12.Location = new Point(414, 460);
+            button12.Name = "button12";
+            button12.Size = new Size(128, 100);
+            button12.TabIndex = 13;
+            button12.Text = "+";
+            button12.UseVisualStyleBackColor = false;
+            button12.Click += OperatorBtn_Click;
+            // 
+            // button13
+            // 
+            button13.BackColor = Color.PowderBlue;
+            button13.Location = new Point(12, 566);
+            button13.Name = "button13";
+            button13.Size = new Size(128, 100);
+            button13.TabIndex = 14;
+            button13.Text = "+/-";
+            button13.UseVisualStyleBackColor = false;
+            button13.Click += NegPosBtn_Click;
+            // 
+            // button14
+            // 
+            button14.BackColor = Color.SteelBlue;
+            button14.Location = new Point(146, 566);
+            button14.Name = "button14";
+            button14.Size = new Size(128, 100);
+            button14.TabIndex = 15;
+            button14.Text = "0";
+            button14.UseVisualStyleBackColor = false;
+            button14.Click += NumBtn_Click;
+            // 
+            // button15
+            // 
+            button15.BackColor = Color.PowderBlue;
+            button15.Location = new Point(280, 566);
+            button15.Name = "button15";
+            button15.Size = new Size(128, 100);
+            button15.TabIndex = 16;
+            button15.Text = ".";
+            button15.UseVisualStyleBackColor = false;
+            button15.Click += DeciBtn_Click;
+            // 
+            // button16
+            // 
+            button16.BackColor = Color.Aqua;
+            button16.Location = new Point(414, 566);
+            button16.Name = "button16";
+            button16.Size = new Size(128, 100);
+            button16.TabIndex = 17;
+            button16.Text = "=";
+            button16.UseVisualStyleBackColor = false;
+            button16.Click += EqualBtn_Click;
+            // 
+            // button17
+            // 
+            button17.BackColor = Color.PowderBlue;
+            button17.Image = (Image)resources.GetObject("button17.Image");
+            button17.Location = new Point(12, 142);
+            button17.Name = "button17";
+            button17.Size = new Size(128, 100);
+            button17.TabIndex = 18;
+            button17.UseVisualStyleBackColor = false;
+            button17.Click += SqrtBtn_Click;
+            // 
+            // button18
+            // 
+            button18.BackColor = Color.Tomato;
+            button18.Location = new Point(146, 142);
+            button18.Name = "button18";
+            button18.Size = new Size(128, 100);
+            button18.TabIndex = 19;
+            button18.Text = "CE";
+            button18.UseVisualStyleBackColor = false;
+            button18.Click += CancelEntryBtn_Click;
+            // 
+            // button19
+            // 
+            button19.BackColor = Color.Tomato;
+            button19.Image = (Image)resources.GetObject("button19.Image");
+            button19.Location = new Point(280, 142);
+            button19.Name = "button19";
+            button19.Size = new Size(128, 100);
+            button19.TabIndex = 20;
+            button19.UseVisualStyleBackColor = false;
+            button19.Click += BckspcBtn_Click;
+            // 
+            // button20
+            // 
+            button20.BackColor = Color.LightSkyBlue;
+            button20.Location = new Point(414, 142);
+            button20.Name = "button20";
+            button20.Size = new Size(128, 100);
+            button20.TabIndex = 21;
+            button20.Text = "/";
+            button20.UseVisualStyleBackColor = false;
+            button20.Click += OperatorBtn_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Desktop;
+            ClientSize = new Size(557, 678);
+            Controls.Add(button20);
+            Controls.Add(button19);
+            Controls.Add(button18);
+            Controls.Add(button17);
+            Controls.Add(button16);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(Tbx_Result);
+            Controls.Add(Tbx_Equation);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Calculator";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox Tbx_Equation;
+        private TextBox Tbx_Result;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Button button17;
+        private Button button18;
+        private Button button19;
+        private Button button20;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+
+namespace EUPINADO
+{
+    public partial class Form1 : Form
+    {
+        public static bool isError = false;
+        public static bool isUndefined = false;
+        bool isOperatorUsed = false;
+        string CurrentExpression = "";
+        public static List<double> Equation = new List<double>();
+        public static List<string> Operations = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DeciBtn_Click(object sender, EventArgs e)
+        {
+            if (isError)
+                return;
+
+            string[] parts = CurrentExpression.Split(new char[] { '+', '-', '*', '/' }, StringSplitOptions.RemoveEmptyEntries);
+            string lastPart = parts.Length > 0 ? parts[parts.Length - 1] : "";
+
+            if (!lastPart.Contains("."))
+            {
+                if (string.IsNullOrEmpty(lastPart))
+                {
+                    CurrentExpression += "0.";
+                    Tbx_Equation.Text += "0.";
+                }
+                else
+                {
+                    CurrentExpression += ".";
+                    Tbx_Equation.Text += ".";
+                }
+            }
+        }
+        private void NegPosBtn_Click(object sender, EventArgs e)
+        {
+            if (isError || string.IsNullOrEmpty(CurrentExpression)) return;
+
+            int lastOperatorIndex = CurrentExpression.LastIndexOfAny(new char[] { '+', '-', '*', '/' });
+            string numberToToggle = lastOperatorIndex == -1 ? CurrentExpression : CurrentExpression.Substring(lastOperatorIndex + 1);
+
+            while (numberToToggle.Contains("---"))
+            {
+                numberToToggle = numberToToggle.Replace("---", "-");
+            }
+            if (numberToToggle.StartsWith("--"))
+            {
+                numberToToggle = "+" + numberToToggle.Substring(2); 
+            }
+            else if (numberToToggle.StartsWith("-"))
+            {
+                numberToToggle = numberToToggle.Substring(1); 
+            }
+            else
+            {
+                numberToToggle = "-" + numberToToggle; 
+            }
+
+            if (lastOperatorIndex == -1)
+            {
+                CurrentExpression = numberToToggle; 
+            }
+            else
+            {
+                CurrentExpression = CurrentExpression.Substring(0, lastOperatorIndex + 1) + numberToToggle;
+            }
+
+            Tbx_Equation.Text = CurrentExpression;
+        }
+
+
+        private void NumBtn_Click(object sender, EventArgs e)
+        {
+            if (isError)
+                return;
+
+            Button numButton = (Button)sender;
+            string value = numButton.Text;
+
+            if (Tbx_Equation.Text == "0" && value != ".")
+            {
+                Tbx_Equation.Text = value;
+            }
+            else
+            {
+                Tbx_Equation.Text += value;
+            }
+            CurrentExpression += value;
+        }
+
+        private void EqualBtn_Click(object sender, EventArgs e)
+        {
+            if (isError)
+                return;
+
+            try
+            {
+                if (string.IsNullOrEmpty(CurrentExpression) || "+-*/".Contains(CurrentExpression[CurrentExpression.Length - 1]))
+                {
+                    Tbx_Result.Text = "Error";
+                    Tbx_Equation.Text = "Incomplete Expression";
+                    isError = true;
+                    return;
+                }
+
+                if (CurrentExpression.Contains("/0"))
+                {
+                    Tbx_Result.Text = "Undefined";
+                    isError = true;
+                    return;
+                }
+
+                var result = new DataTable().Compute(CurrentExpression, null);
+
+                Tbx_Result.Text = Math.Round(Convert.ToDouble(result), 2).ToString();
+            }
+            catch
+            {
+                Tbx_Result.Text = "Error";
+                isError = true;
+            }
+        }
+
+        private void BckspcBtn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(Tbx_Equation.Text) || Tbx_Equation.Text == "0")
+            {
+                return;
+            }
+
+            CurrentExpression = CurrentExpression.Length > 1
+                ? CurrentExpression.Substring(0, CurrentExpression.Length - 1)
+                : "";
+
+            Tbx_Equation.Text = Tbx_Equation.Text.Length > 1
+                ? Tbx_Equation.Text.Substring(0, Tbx_Equation.Text.Length - 1)
+                : "0";
+        }
+
+        private void CancelEntryBtn_Click(object sender, EventArgs e)
+        {
+            ResetCalculator();
+        }
+
+        private void OperatorBtn_Click(object sender, EventArgs e)
+        {
+            if (isError)
+                return;
+
+            Button OperationBtn = (Button)sender;
+            string operatorValue = OperationBtn.Text;
+
+            if (!string.IsNullOrEmpty(CurrentExpression) && !"+-*/".Contains(CurrentExpression[CurrentExpression.Length - 1]))
+            {
+                CurrentExpression += operatorValue;
+                Tbx_Equation.Text += operatorValue;
+                isOperatorUsed = true;
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(CurrentExpression) && "+-*/".Contains(CurrentExpression[CurrentExpression.Length - 1]))
+                {
+                    CurrentExpression = CurrentExpression.Substring(0, CurrentExpression.Length - 1) + operatorValue;
+                    Tbx_Equation.Text = Tbx_Equation.Text.Substring(0, Tbx_Equation.Text.Length - 1) + operatorValue;
+                }
+                else
+                {
+                    Tbx_Result.Text = "Error";
+                    Tbx_Equation.Text = "Invalid Operator Sequence";
+                    isError = true;
+                }
+            }
+        }
+
+        private void SqrtBtn_Click(object sender, EventArgs e)
+        {
+            if (isError) return;
+
+            string equation = Tbx_Equation.Text;
+
+            char[] operators = { '+', '-', '*', '/' };
+            int operatorIndex = equation.IndexOfAny(operators);
+
+            if (operatorIndex != -1)
+            {
+                foreach (char op in operators)
+                {
+                    if (equation.Contains(op))
+                    {
+                        string[] parts = equation.Split(op);
+                        if (parts.Length == 2 && double.TryParse(parts[0], out double num1) && double.TryParse(parts[1], out double num2))
+                        {
+                            num1 = Math.Sqrt(num1);  
+                            double result = 0;
+
+                            switch (op)
+                            {
+                                case '+': 
+                                    result = num1 + num2; break;
+                                case '-': 
+                                    result = num1 - num2; break;
+                                case '*': 
+                                    result = num1 * num2; break;
+                                case '/':
+                                    if (num2 == 0) 
+                                    { 
+                                        Tbx_Result.Text = "Error"; isError = true;
+                                        return; 
+                                    }
+                                    result = num1 / num2;
+                                    break;
+                            }
+
+                            Tbx_Result.Text = Math.Round(result, 2).ToString();
+                            Tbx_Equation.Text = "√" + parts[0] + " " + op + " " + parts[1];
+                            return;
+                        }
+                    }
+                }
+                Tbx_Result.Text = "Invalid Input"; 
+            }
+            else if (double.TryParse(equation, out double num))
+            {
+                if (num < 0)
+                {
+                    Tbx_Result.Text = "Invalid Input";  
+                    isError = true;
+                }
+                else
+                {
+                    Tbx_Result.Text = Math.Round(Math.Sqrt(num), 2).ToString();
+                    Tbx_Equation.Text = "√" + equation;
+                }
+            }
+            else
+            {
+                Tbx_Result.Text = "Invalid Input";  
+                isError = true;
+            }
+        }
+        private void ResetCalculator()
+        {
+            Tbx_Equation.Text = "0";
+            CurrentExpression = "";
+            Tbx_Result.Text = "";
+            Equation.Clear();
+            Operations.Clear();
+            isUndefined = false;
+            isError = false;
+            isOperatorUsed = false;
+        }
+    }
+}
